493bfe8ad6f36409923b1bddd2c88c5adcdcb57f
Tichin Wu <twu38@mail.ccsf.edu>
2023-07-11 17:09:25 -0700

    get the response from API

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+'use strict';
 console.log("Let's get this party started!");
+/** create an async function where we click we can search giphy */
+let $form = $('#search-form');
+let $searchTerm = $('#retrieve-text');
+$form.on('submit', search);
+
+function search(e) {
+
+  e.preventDefault();
+  getGiphy();
+}
+
+
+const GIPHY_URL = 'http://api.giphy.com/v1/gifs/search';
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+const CONFIG = { params: { q: $searchTerm.val(), api_key: API_KEY } };
+
+async function getGiphy() {
+
+  let response = await axios.get(GIPHY_URL, CONFIG);
+  console.log('response', response);
+}
